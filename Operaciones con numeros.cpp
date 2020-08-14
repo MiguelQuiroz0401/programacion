da86edd14d56aeaeb0304c9a494528007abd2684
MiguelQuiroz0401 <69353543+MiguelQuiroz0401@users.noreply.github.com>
2020-08-14 10:11:20 -0400

    Add files via upload

---
+++ Operaciones con numeros.cpp
@@ -0,0 +1,63 @@
+/******************************************************************************
+
+                              Online C++ Compiler.
+               Code, Compile, Run and Debug C++ program online.
+Write your code in this editor and press "Run" button to compile and execute it.
+
+*******************************************************************************/
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int a,b,c;
+    cout<< "SUMA"<<endl;
+    cout<< "El primer numero es: ";
+    cin>>a;
+    cout<< "El segundo numero es: ";
+    cin>>b;
+    c= a+b;
+    cout<< "El resultado de la suma es: " <<c<<endl;
+    
+    cout<< "//////////////////////////"<<endl;
+    cout<< "RESTA"<<endl;
+    cout<< "El primer numero es: ";
+    cin>> a;
+    cout<< "El segundo numero es: ";
+    cin>> b;
+    c= a-b;
+    cout<< "El resultado de la resta es: "<< c<<endl;
+    
+     cout<< "//////////////////////////"<<endl;
+    cout<< "Multiplicacion"<<endl;
+    cout<< "El primer numero es: ";
+    cin>> a;
+    cout<< "El segundo numero es: ";
+    cin>> b;
+    c= a*b;
+    cout<< "El resultado de la multiplicacion es: "<< c<<endl;
+    
+     cout<< "//////////////////////////"<<endl;
+    cout<< "Division"<<endl;
+    cout<< "El primer numero es: ";
+    cin>> a;
+    cout<< "El segundo numero es: ";
+    cin>> b;
+    c= a/b;
+    cout<< "El resultado de la division es: "<< c<<endl;
+    
+      cout<< "//////////////////////////"<<endl;
+    cout<< "Division"<<endl;
+    cout<< "El primer numero es: ";
+    cin>> a;
+    cout<< "El segundo numero es: ";
+    cin>> b;
+    c= a%b;
+    cout<< "El modulo es: "<< c<<endl;
+    
+    
+    
+    return 0;
+}
