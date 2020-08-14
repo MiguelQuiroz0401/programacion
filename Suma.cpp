d79e0a305ff2c0067b72a72a08bad077efc8c4ee
MiguelQuiroz0401 <69353543+MiguelQuiroz0401@users.noreply.github.com>
2020-08-14 09:39:10 -0400

    Add files via upload

---
+++ Suma.cpp
@@ -0,0 +1,24 @@
+/******************************************************************************
+
+                              Online C++ Compiler.
+               Code, Compile, Run and Debug C++ program online.
+Write your code in this editor and press "Run" button to compile and execute it.
+
+*******************************************************************************/
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int a,b,c;
+    cout<< "El primer numero es: ";
+    cin>>a;
+    cout<< "El segundo numero es: ";
+    cin>>b;
+    c= a+b;
+    cout<< "El resultado es: " <<c;
+    
+    return 0;
+}
