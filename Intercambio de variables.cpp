be18ee9babb4e22eacdd85df4dce8a3c1d8df105
MiguelQuiroz0401 <69353543+MiguelQuiroz0401@users.noreply.github.com>
2020-10-08 21:44:57 -0400

    Add files via upload

---
+++ Intercambio de variables.cpp
@@ -0,0 +1,25 @@
+#include "stdafx.h"
+#include <iostream>
+#include "conio.h"
+
+using namespace std;
+
+void VariableCambio(float a,float b);  
+
+void main()
+{
+	float a=1,b=2;
+	cout<<"Ingrese a: ";
+
+	VariableCambio(a,b);
+	cout<<a<<"  "<<b;
+	getch();
+	
+}
+
+void VariableCambio(float a,float b)
+{
+	float y;
+	y=x;
+	return y;
+}
